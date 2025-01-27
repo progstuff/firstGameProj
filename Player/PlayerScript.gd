48ce3b7fde3815623240d03f13a3ad0e45983af3
PROGSTUDY <yerdnavozorom@gmail.com>
2025-01-27 22:09:02 +0300

    Добавил процедурную генерацию карты (частично: только один вид поверхности, + надо доработать алгоритм определения индекса генерируемого куска карты)

--- Player/PlayerScript.gd
+++ Player/PlayerScript.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @onready var anim_state = $AnimationTree.get("parameters/playback")
 @onready var anim_tree = $AnimationTree
 
-enum states {MOVE, IDLE, DEAD}
+enum states {MOVE, IDLE, DEAD, DODGE}
 var current_state = states.MOVE
 
 var input_movement = Vector2.ZERO
@@ -16,11 +16,8 @@ func _ready() -> void:
 	$SwordScene.visible = true
 	
 func _physics_process(_delta: float) -> void:
-	
-	match current_state:
-		states.MOVE:
-			move()
-
+	move()
+			
 func move() -> void:
 	
 	input_movement = Input.get_vector("left", "right", "up", "down")
@@ -31,6 +28,7 @@ func move() -> void:
 		anim_state.travel("Walk")
 		
 		if(Input.is_action_just_pressed("shift")):
+			current_state = states.DODGE
 			currentPlayerSpeed = shiftSpeed
 			$CollisionShape2D.disabled = true
 			$Timer.start()
@@ -38,16 +36,17 @@ func move() -> void:
 		velocity = input_movement  * currentPlayerSpeed
 	
 	if input_movement == Vector2.ZERO:
+		current_state = states.IDLE
 		anim_state.travel("Idle")
 		velocity = Vector2.ZERO
 	
 	move_and_slide()
 
-
 func _on_timer_timeout() -> void:
+	current_state = states.MOVE
 	currentPlayerSpeed = walkSpeed
 	$CollisionShape2D.disabled = false
-	
 
-	
-	
+func _on_player_area_entered(area: Area2D) -> void:
+	if(area.name == "enemy"):
+		pass
--- main.gd
+++ main.gd
@@ -1,13 +1,23 @@
 extends Node2D
 var enemyScene = load("res://Enemy/EnemyScene.tscn")
+var playerScene = load("res://Player/PlayerScene.tscn")
+var mapScene = load("res://Map/MapScene.tscn")
+
+var CHUNK_HEIGHT = 40 * 16
+var CHUNK_WIDTH = 72 * 16
+var tileMap = {Vector2(0, 0): true}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:	
-	var playerPosition = $PlayerScene/CharacterBody2D.position
+func _process(delta: float) -> void:
+	if($Player.get_child_count() == 0):
+		return
+		
+	var player = $Player.get_child(0)
+	var playerPosition = player.get_node("CharacterBody2D").position
 	var enemies = $Mobs.get_children()
 	for enemy in enemies:
 		var enemyCharacter = enemy.get_node("CharacterBody2D")
@@ -18,7 +28,7 @@ func _process(delta: float) -> void:
 			var enemyPosition = enemyCharacter.position
 			var direction = enemyPosition.direction_to(playerPosition)
 			
-			if((enemyPosition - playerPosition).length() < 30):
+			if((enemyPosition - playerPosition).length() < 3):
 				direction = Vector2.ZERO
 			if(enemyCharacter.live <= 0):
 				direction = Vector2.ZERO
@@ -27,16 +37,49 @@ func _process(delta: float) -> void:
 			
 			enemyCharacter.input_movement = direction
 			enemyCharacter.position = enemyCharacter.position + direction*delta*enemyCharacter.speed
-			
+	
+	generateTerrain()		
 
+func generateTerrain() -> void:
+	if($Player.get_child_count() == 0):
+		return
+		
+	var player = $Player.get_child(0)
+	var playerPosition = player.get_node("CharacterBody2D").position
+	
+	playerPosition = playerPosition + Vector2(200, -200);
+	
+	var x = floor(playerPosition.x / CHUNK_WIDTH)
+	var y = floor(playerPosition.y / CHUNK_HEIGHT)
+	var chunkCoord = Vector2(x, y)
+	
+	if(!tileMap.has(chunkCoord)):
+		var chunk = mapScene.instantiate()
+		chunk.position.x = x * CHUNK_WIDTH
+		chunk.position.y = y * CHUNK_HEIGHT
+		$Map.add_child(chunk)
+		tileMap[chunkCoord] = true
+		
+	
+	
 func _on_main_menu_start_game() -> void:
 	var screen_size = get_viewport().get_visible_rect().size
 	var startPlayerPosition = screen_size/2
-	$PlayerScene.visible = true
-	$PlayerScene.get_node("CharacterBody2D").position = startPlayerPosition
+	var chunk = mapScene.instantiate()
+	$Map.add_child(chunk)
+	
+	var player = playerScene.instantiate()
+	$Player.add_child(player)
+	
+	player.get_node("CharacterBody2D").position = startPlayerPosition
 	$MobSpawnTimer.start()
 
 func _on_mob_spawn_timer_timeout() -> void:
+	if($Player.get_child_count() == 0):
+		return
+		
+	var player = $Player.get_child(0)
+	var playerPosition = player.get_node("CharacterBody2D").position
 	
 	if($Mobs.get_child_count() < 100):
 		var rand = RandomNumberGenerator.new()
@@ -47,13 +90,21 @@ func _on_mob_spawn_timer_timeout() -> void:
 			var enemyObject = enemyScene.instantiate()
 			
 			rand.randomize()
-			var x = rand.randf_range(0, screen_size.x)
+			var x = rand.randf_range(0, screen_size.x) + playerPosition.x - screen_size.x/2
 			rand.randomize()
-			var y = rand.randf_range(0, screen_size.y)
+			var y = rand.randf_range(0, screen_size.y) + playerPosition.y - screen_size.y/2
 			var randomPos = Vector2(x,y)
 			
 			enemyObject.get_node("CharacterBody2D").position = randomPos
-			enemyObject.get_node("CharacterBody2D").speed = rand.randi_range(20, 80)
+			enemyObject.get_node("CharacterBody2D").speed = rand.randi_range(50, 160)
 			$Mobs.add_child(enemyObject)
+
+
+func _on_debug_timer_timeout() -> void:
+	if($Player.get_child_count() == 0):
+		return
 		
-		
+	var player = $Player.get_child(0)
+	var playerPosition = player.get_node("CharacterBody2D").position
+	
+	print(playerPosition)
